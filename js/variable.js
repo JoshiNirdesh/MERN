04048004789fa96d2c381553b6be55af63afcdf4
JoshiNirdesh <joshinirdesh10@gmail.com>
2025-03-09 09:26:09 +0545

    variable in js

--- js/variable.js
+++ js/variable.js
@@ -2,3 +2,24 @@ let a=50;
 let b=60;
 
 console.log(a+b);
+
+var c=5;
+var c=6;
+var c=10;
+
+console.log(c);
+
+let d=30;
+
+{
+    let d=20;
+    console.log(d);
+}
+console.log(d);
+
+
+const q=4;
+
+const w=5;
+
+console.log(q);
