0e47cc32e1f81b1745119bfedd7f43f119c54196
JoshiNirdesh <joshinirdesh10@gmail.com>
2025-03-18 14:49:59 +0545

    promise

--- js/promise.js
+++ js/promise.js
@@ -3,17 +3,38 @@
 // }
 // console.log("Multply", mul(2,4));
 
-const mul=(x,y)=>{
-    return x*y;
-}
-console.log("Multply", mul(2,4));
+// const mul=(x,y)=>{
+//     return x*y;
+// }
+// console.log("Multply", mul(2,4));
 
-function car(brand){
+// function car(brand){
+//     setTimeout(()=>{
+//         console.log(brand)
+//     },1000)
+//     return brand;
+// }
+
+// const car1=car("toyata");
+// console.log("Brand", car1);
+
+//promise
+
+const promise= new Promise((resolve, reject)=>{
     setTimeout(()=>{
-        console.log(brand)
-    },1000)
-    return brand;
-}
+        reject("Internal server error || error");
+        resolve({
+            data:{
+                name:"Nirdesh",
+                email:"nin@gmail.com"
+            },
+        })
+    },2000)
 
-const car1=car("toyata");
-console.log("Brand", car1);
+});
+promise.then((data)=>{
+    console.log(data);
+    
+}).catch((error)=>{
+    console.log("error");
+});
