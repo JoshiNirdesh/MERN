8d381c2e3ae6ee6521262f5a0e119d62858bc2c1
JoshiNirdesh <joshinirdesh10@gmail.com>
2025-03-18 14:18:52 +0545

    function2

--- js/callBack.js
+++ js/callBack.js
@@ -1,11 +1,8 @@
-// call back function 
-const nestedArray=(arr, action)=>{
-    return arr[i]=action
-
-}
 
 const add =(x,y)=>{
     return x+y;
 }
 const addNumber=add(2,3);
-console.log("Sum: ", addNumber)
+console.log("Sum: ", addNumber);
+
+// call backfunction
