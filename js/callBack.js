5e3d685c906b314ac4f9fb149570ba2a571c2e98
JoshiNirdesh <joshinirdesh10@gmail.com>
2025-03-18 14:23:04 +0545

    call back function

--- js/callBack.js
+++ js/callBack.js
@@ -1,8 +1,19 @@
 
-const add =(x,y)=>{
-    return x+y;
-}
-const addNumber=add(2,3);
-console.log("Sum: ", addNumber);
+// const add =(x,y)=>{
+//     return x+y;
+// }
+// const addNumber=add(2,3);
+// console.log("Sum: ", addNumber);
 
 // call backfunction
+
+function fetchData(c){
+    console.log("Api is fetching");
+    setTimeout(()=>{
+        c("api succesfully fetched");
+    },2000);
+}
+fetchData((m)=>{
+    console.log(m);
+    
+})
